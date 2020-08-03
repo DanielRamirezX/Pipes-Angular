942daa0fd98521948510eee00fadedfb643d649e
DanielRamirezX <daniel.carlos98@hotmail.com>
2020-08-02 20:46:02 -0500

    Proyecto Pipes Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Capitan America';
+  nombre2 = 'dAniEl RamIreZ';
+
+  arreglo = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ];
+  pi: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 12345;
+  fecha = new Date();
+  idioma = 'es';
+
+  videoURL = 'https://www.youtube.com/embed/EUX6lXTX9zQ';
+
+  valorPromesa = new Promise<string>( (resolve) => {
+
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20,
+    }
+  };
+
 }
